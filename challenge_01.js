cc5adb73ea51152909d1b20fc285c0d8e9102489
Trandoner <trand_oner@gmx.de>
2023-07-13 13:11:18 +0200

    Challenge 01 Update

--- challenge_01.js
+++ challenge_01.js
@@ -9,3 +9,15 @@ Geben Sie die Summe der Zahlen in die Konsole aus:
 "Die Summe der Zahlen ist: summe"
 */
 
+// Eingabe der ersten Zahl
+const zahl1 = parseFloat(prompt("Geben Sie die erste Zahl ein:"));
+
+// Eingabe der zweiten Zahl
+const zahl2 = parseFloat(prompt("Geben Sie die zweite Zahl ein:"));
+
+// Berechnung der Summe
+const summe = zahl1 + zahl2;
+
+// Ausgabe der Summe
+console.log("Die Summe der Zahlen ist:", summe);
+
